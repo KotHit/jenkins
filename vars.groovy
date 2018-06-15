a753c7babef4fdce5bf19339ca969f2ae11e4c95
PKudrynskyi <PKudrynskyi@luxoft.com>
2018-06-15 12:24:40 +0300

    edit

--- vars.groovy
+++ vars.groovy
@@ -1,1 +1,7 @@
-env.CUSTOM="Hello"
+import hudson.EnvVars
+import hudson.model.Environment
+
+def build = Thread.currentThread().executable
+def vars = [CUSTOM1: 'value1', custom2: 'value2']
+
+build.environments.add(0, Environment.create(new EnvVars(vars)))
