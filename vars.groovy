b95e753944801d4488c5ce5fa9bbb236f1445321
PKudrynskyi <PKudrynskyi@luxoft.com>
2018-06-15 12:39:53 +0300

    few

--- vars.groovy
+++ vars.groovy
@@ -3,5 +3,5 @@ import hudson.model.Environment
 
 def build = Thread.currentThread().executable
 def vars = [CUSTOM1: 'value1', custom2: 'value2']
-
+println "hi"
 build.environments.add(0, Environment.create(new EnvVars(vars)))
